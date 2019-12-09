a589e8f96f887ecd538c7fe792ceb73e3497e948
PaulineGrn <pauline.gernigon@orange.fr>
2019-12-09 10:05:11 +0100

    SQLPart2

--- script.sql
+++ script.sql
@@ -1,43 +1,48 @@
 --Partie 2
 --Ex1
 --Utilisation de la BD webdevelopement
-USE `webdevelopement` ;
+USE `webDevelopment` ;
 --Création de la table languages
 CREATE TABLE `languages`
---Création de 2 colonnes "id" et "tool". Pour la première avec un type INT et une clé primaire et une auto incrémentation
-( id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  --Création d'un type VARCHAR avec le nombre de caractère entre parenthèse
-  tool VARCHAR(50)
-)
+--Création de 2 colonnes "id" et "languages". Pour la première avec un type INT et une clé primaire et une auto incrémentation
+(
+  `id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  --Création d'un type VARCHAR avec le nombre de caractère entre parenthèse (chaîne de caractères)
+  `languages` VARCHAR(50)
+);
 --Ex2
-USE `webdevelopement`;
+USE `webdevelopment`;
 CREATE TABLE `tools`
-( id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-   tool VARCHAR(50)
- )
+(
+    `id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+   `tool` VARCHAR(50)
+ );
  --Ex3
  --Utilisation de la BD webdevelopement
- USE `webdevelopement`;
+ USE `webDevelopment`;
  --Création de la table si elle n'existe pas (frameworks)
 CREATE TABLE IF NOT EXISTS `frameworks`
 --Création de 2 colonnes 'id' et 'name' avec 2 type "Int" et "VARCHAR". Et enfin avec la clé primaire et l'auto incrémentation
-(  id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-   name VARCHAR(100)
- )
+(
+  `id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(100)
+ );
  --Ex4
+ USE `webDevelopment`;
  --Suppression de la table tools
- DROP TABLE `tools`
+ DROP TABLE IF EXISTS `tools`;
  --Ex5
  --Utilisation de la BD webdevelopement
- USE `webdevelopement`;
+ USE `webDevelopment`;
  --Création de la table clients
  CREATE TABLE `clients`
  --Création de plusieurs colonnes
- ( id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  lastname VARCHAR (50),
-  firstname VARCHAR(50),
-  birthdate DATE,
-  address VARCHAR (100),
-  phone INT,
-  mail VARCHAR(150)
-  )
+ (
+  `id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `lastname` VARCHAR (50),
+  `firstname` VARCHAR(50),
+  `birthdate` DATE,
+  `address` VARCHAR (100),
+  `phone` INT,
+  `mail`VARCHAR(150)
+);
